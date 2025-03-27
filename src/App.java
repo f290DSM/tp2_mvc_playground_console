baad41976c899198af283d125a06a4d604e67754
BZR4 <BZR4@users.noreply.github.com>
2025-03-26 22:13:01 -0300

    buscar por e-mail, buscar todos

--- src/App.java
+++ src/App.java
@@ -1,19 +1,28 @@
 import java.sql.Connection;
 import java.sql.ResultSet;
 
+import model.ContatoVO;
+import model.dao.ContatoDAO;
 import model.factory.ConnectionFactory;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // Connection connection = ConnectionFactory.getConnection();
-        // System.out.println(connection);
+        Connection connection = ConnectionFactory.getConnection();
+        ContatoDAO dao = new ContatoDAO(connection);
+
+        ContatoVO c1 = new ContatoVO(null,
+                "Alfredo Silva",
+                "a.silva@gmail.com",
+                "(19)98888-7777");
+
+        dao.salvar(c1);
 
         // ResultSet rst = connection
-        //         .createStatement()
-        //         .executeQuery("select version() as versao;");
+        // .createStatement()
+        // .executeQuery("select version() as versao;");
 
         // if (rst.next()) {
-        //     System.out.println(rst.getString("versao"));
+        // System.out.println(rst.getString("versao"));
         // }
 
         // rst.close();
--- src/model/dao/ContatoDAO.java
+++ src/model/dao/ContatoDAO.java
@@ -1,29 +1,100 @@
 package model.dao;
 
+import java.lang.Thread.State;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import model.ContatoVO;
-import model.factory.ConnectionFactory;
 
 public class ContatoDAO {
 
+    private final Connection connection;
+    private final Logger log;
+
+    public ContatoDAO(Connection connection) {
+        this.connection = connection;
+        this.log = Logger.getLogger(ContatoDAO.class.getName());
+    }
+
     public void salvar(ContatoVO contato) {
         try {
-            Connection connection = ConnectionFactory.getConnection();
             String query = """
                     INSERT INTO contatos (nome, email, telefone) VALUES
                     ('%s', '%s', '%s');
                     """;
 
-                    Statement statement = connection.createStatement();
-                    statement.execute(String.format(query,
-                            contato.getNome(),
-                            contato.getEmail(),
-                            contato.getTelefone()));
-        } catch (Exception e) {
+            String sql = query.formatted(
+                    contato.getNome(),
+                    contato.getEmail(),
+                    contato.getTelefone());
+
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+
+            log.info("Contato criado com sucesso");
+
+            statement.close();
+        } catch (SQLException e) {
+            log.log(Level.SEVERE, "Falha ao salvar novo contato.", e);
             throw new RuntimeException(e);
         }
+    }
+
+    ContatoVO atualizar(ContatoVO contato) {
+        return null;
+
+    }
+
+    void excluir(Integer id) {
+
+    }
+
+    public ContatoVO buscarPorEmail(String email) {
+        String sql = "SELECT id, nome, email, telefone FROM contatos WHERE email = " + email;
+        try (Statement stm = connection.createStatement();
+                ResultSet rst = stm.executeQuery(sql)) {
+            if (rst.next()) {
+                ContatoVO contatoVO = new ContatoVO(
+                        rst.getInt("id"),
+                        rst.getString("nome"),
+                        rst.getString("email"),
+                        rst.getString("telefone"));
+                log.info("Consulta realizada com sucesso.");
+                return contatoVO;
+            }
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "Falha ao consultar contatos", e);
+        }
+        return null;
+    }
+
+    public List<ContatoVO> buscarTodos() throws SQLException {
+        List<ContatoVO> contatos = new ArrayList<>();
+        String sql = "SELECT id, nome, email, telefone FROM contatos";
+
+        try (Statement stm = connection.createStatement();
+                ResultSet rst = stm.executeQuery(sql);) {
+
+            while (rst.next()) {
+                ContatoVO contatoVO = new ContatoVO(
+                        rst.getInt("id"),
+                        rst.getString("nome"),
+                        rst.getString("email"),
+                        rst.getString("telefone"));
+                contatos.add(contatoVO);
+            }
+            log.info("Consulta realizada com sucesso.");
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "Falha ao consultar contatos", e);
+        }
+        return contatos;
     }
 
 }
